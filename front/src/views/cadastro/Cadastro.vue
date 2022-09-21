40511eb0588ecc5162d2c287c93e6fc31919096e
RaphaLella <raphaellabrandaojacques@hotmail.com>
2022-09-20 22:59:09 -0400

    Tela de cadastro

--- front/src/views/cadastro/Cadastro.vue
+++ front/src/views/cadastro/Cadastro.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    Esta é uma página de cadastro
+  <div class="container-cadastro"
+  >
+    <banner :titulo="bannerTitulo">
+      <template v-slot:subtitulo>
+        <div>
+          Escolha um jogador para se cadastrar
+        </div>
+      </template>
+    </banner>
+
+    <v-conteiner>
+      <v-row class="card-cadastro-conteiner"
+      >
+        <card-cadastro/>
+        <card-cadastro/>
+      </v-row>
+    </v-conteiner>
+    
   </div>
 </template>
 
 <script>
-// export default {
+  import Banner from '../../components/Banner.vue'
+  import CardCadastro from '../../components/CardCadastro.vue'
 
-// }
+  export default {
+    name: 'cadastro',
+    components: {
+      Banner,
+      CardCadastro
+    },
+    data() {
+      return {
+        bannerTitulo: "Cadastro",
+        dialog: false
+      }
+    }
+    
+  }
+
 </script>
 
 <style>
-
+.container-cadastro {
+  background-image: url("../../assets/grama.jpg");
+  background-repeat: repeat;
+  background-color: #DDDDDD;
+  /*padding-bottom: 4.8em;*/
+  /* width: 100vw; */
+  height: 100vh; /*88 para não ter rolamento*/
+}
+.card-cadastro-conteiner{
+  display: flex;
+  justify-content: space-around;
+}
 </style>
